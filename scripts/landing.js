6883c03270fa722653d1391f3c724ec6ba0c8530
lusmtan <njgarcia@student.apc.edu.ph>
2024-11-27 13:18:14 +0800

    Animation v1

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,6 +3,11 @@ const signUp = document.getElementById("btn-signUp");
 
 const divSignIn = document.getElementsByClassName("signIn")[0];
 const divSignUp = document.getElementsByClassName("signUp")[0];
+const rdSignIn = document.getElementsByClassName("right-signIn")[0];
+const lsdSignIn = document.getElementsByClassName("left-signIn")[0];
+const ldSignUp = document.getElementsByClassName("left-signUp")[0];
+
+const rdSignUp = document.getElementsByClassName("right-signUp")[0];
 
 // Set initial active class
 divSignIn.classList.add("active");
@@ -13,6 +18,9 @@ signUp.addEventListener("click", changeToSignUp);
 function changeToSignUp() {
     if (divSignIn.classList.contains("active")) {
         divSignIn.classList.remove("active");
+        rdSignUp.classList.add("active");
+        lsdSignIn.classList.add("active");
+        rdSignIn.classList.add("active");
         divSignUp.classList.add("active");
     } else {
         console.log("error in changing div.");
@@ -22,6 +30,8 @@ function changeToSignUp() {
 function changeToSignIn() {
     if (divSignUp.classList.contains("active")) {
         divSignUp.classList.remove("active");
+        lsdSignIn.classList.remove("active");
+        rdSignIn.classList.remove("active");
         divSignIn.classList.add("active");
     } else {
         console.log("error in changing div.");
